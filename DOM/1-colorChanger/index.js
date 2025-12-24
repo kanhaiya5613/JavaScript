a42cbb31b110542d5876748b4cbebd3b66e96ada
kanhaiya kumar <kk7077574@gmail.com>
2025-12-25 00:30:38 +0530

    Add color changer project

---
+++ DOM/1-colorChanger/index.js
@@ -0,0 +1,13 @@
+const buttons = document.querySelectorAll('.button')
+const body = document.querySelector('body')
+console.log(buttons)
+buttons.forEach(function (button){
+    console.log(button)
+    button.addEventListener('click',function(e){
+        console.log(e)
+        console.log(e.target)
+        if(e.target.id){
+            body.style.backgroundColor = e.target.id;
+        }
+    })
+})
